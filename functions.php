192998eb4c985fe9277143e5ec5d5b0cc27f35c4
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-07-27 06:15:29 +0900

    ペットショップ2課題完了

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // ファイルの読み込み
 require_once __DIR__ . '/config.php';
@@ -7,16 +7,12 @@ require_once __DIR__ . '/config.php';
 function connect_db()
 {
     try {
-        return new PDO(
-            DSN,
-            USER,
-            PASSWORD,
-            [PDO::ATTR_ERRMODE =>
-            PDO::ERRMODE_EXCEPTION]
-        );
+        return new PDO(DSN, USER, PASSWORD, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
     } catch (PDOException $e) {
         echo $e->getMessage(); #本来はこの書き方はしない。セキュリティー上、危険な書き方。
-        exit;
+        exit();
     }
 }
 
@@ -27,3 +23,56 @@ function h($str)
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+// 本日のご紹介ペット抽出
+function select_animals($keyword)
+{
+    // データベースに接続
+    $dbh = connect_db();
+
+    // SQL文
+    $sql = <<<EOM
+SELECT
+    *
+FROM
+    animals
+WHERE
+    description LIKE :keyword;
+EOM;
+
+    // パラメータ設定
+    $keyword_param = '%' . $keyword . '%';
+    // プリペアドステートメント準備
+    $stmt = $dbh->prepare($sql);
+    // パラメータのバインド
+    $stmt->bindValue(':keyword', $keyword_param, PDO::PARAM_STR);
+    // プリペアドステートメント実行(これでSQL文が実行される)
+    $stmt->execute();
+
+    $select_animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $select_animals;
+}
+
+// これは使わない関数
+// 全データ読み込み
+function all_select_animals()
+{
+    // データベースに接続
+    $dbh = connect_db();
+
+    // animalsテーブルを選択
+    $sql = <<<EOM
+SELECT
+    *
+FROM
+    animals
+EOM;
+
+    //準備
+    $stmt = $dbh->prepare($sql);
+
+    // 実行
+    $stmt->execute();
+
+    // 連想配列でデータベースからデータを取得する
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
--- index.php
+++ index.php
@@ -1,9 +1,54 @@
-<?php 
+<?php
 
 // 関数読み込み
 require_once __DIR__ . '/functions.php';
 
 // データベースに接続
 $dbh = connect_db();
-echo '接続に成功しました！<br>';
+// echo '接続に成功しました！<br>';
 
+// 初期化
+$keyword = '';
+
+// 入力を取得
+$keyword = filter_input(INPUT_GET, 'keyword');
+// 確認
+// var_dump($keyword);
+// キーワードを含むanimalを取得
+$select_animals = select_animals($keyword);
+
+// var_dump($select_animals);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet_shop</title>
+</head>
+<body>
+    <h1>本日のご紹介ペット！</h1>
+    <form action="" method="GET">
+        <label for="">キーワード:</label>
+        <input type="text" name="keyword" placeholder="キーワードの入力" >
+        <input type="submit" value="検索">
+        <ul style='list-style-type: none; padding-left: 0;'>
+        <?php if (!empty($keyword)): ?>
+            <?php for ($idx = 0; $idx < count($select_animals); $idx++): ?>
+                <li><?= $select_animals[$idx]['type'] .
+                    'の' .
+                    $select_animals[$idx]['classification'] .
+                    'ちゃん' ?></li>
+                <li><?= $select_animals[$idx]['description'] ?></li>
+                <li><?= $select_animals[$idx]['birthday'] . ' 生まれ' ?></li>
+                <li><?= '出身地 ' . $select_animals[$idx]['birthplace'] ?></li>
+                <hr>
+            <?php endfor; ?>
+        <?php endif; ?>
+        </ul>
+        
+    </form>
+</body>
+</html>
